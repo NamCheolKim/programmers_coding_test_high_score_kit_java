4469293583a5efc618d916dbad74e069df71362f
NamCheolKim <pkeowkd@naver.com>
2021-03-28 04:38:54 +0900

    programmers_coding_test_high_score_kit_java;

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,3 @@
 class Solution {
-    static int solution(String name) {
 
-        int answer = 0;
-        int len = name.length();
-        int minMove = len - 1;
-
-        for(int i = 0; i < len; i++){
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-
-            int nextChar = i + 1;
-
-            while (nextChar < len && name.charAt(nextChar) == 'A'){
-                nextChar += 1;
-            }
-            int move = i * 2 + len - nextChar;
-            minMove = Math.min(minMove, move);
-        }
-        answer += minMove;
-        return answer;
-    }
 }
